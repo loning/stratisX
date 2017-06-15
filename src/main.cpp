502a4b322ff48c29d69a4a8fa84cf69c91f4edf9
Loning <aloning@gmail.com>
2017-06-15 22:28:01 +0800

    no stake reward

--- src/main.cpp
+++ src/main.cpp
@@ -1005,7 +1005,7 @@ int64_t GetProofOfWorkReward(int64_t nFees)
 // miner's coin stake reward
 int64_t GetProofOfStakeReward(const CBlockIndex* pindexPrev, int64_t nCoinAge, int64_t nFees)
 {
-    return COIN + nFees;
+    return nFees;
 }
 
 static const int64_t nTargetTimespan = 16 * 60;  // 16 mins
