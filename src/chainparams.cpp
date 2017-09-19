c3402597bc471a01b1e2c117ac6e0ab29e61730f
Loning <aloning@gmail.com>
2017-09-19 16:29:44 +0800

    change magic code

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -71,7 +71,7 @@ public:
         // The message start string is designed to be unlikely to occur in normal data.
         // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
         // a large 4-byte int at any alignment.
-        pchMessageStart[0] =0x12;
+        pchMessageStart[0] =0x22;
         pchMessageStart[1] =0x12;
         pchMessageStart[2] =0x03;
         pchMessageStart[3] =0x64;
